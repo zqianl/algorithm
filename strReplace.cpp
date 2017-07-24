d365478e2117f30a9b46146dfade2ecf048ea70e
zqianl <1229170125@qq.com>
2017-07-24 18:46:48 +0800

    replace all matched strings using newStr

---
+++ strReplace.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+string&   replace_all(string&   str, const   string&   old_value, const   string&   new_value)
+{
+	while (true)   {
+		string::size_type   pos(0);
+		if ((pos = str.find(old_value)) != string::npos)
+			str.replace(pos, old_value.length(), new_value);
+		else   break;
+	}
+	return   str;
+}
+string&   replace_all_distinct(string&   str, const   string&   old_value, const   string&   new_value)
+{
+	for (string::size_type pos(0); pos != string::npos; pos += new_value.length())   {
+		if ((pos = str.find(old_value, pos)) != string::npos)
+			str.replace(pos, old_value.length(), new_value);
+		else   break;
+	}
+	return   str;
+}
+int   main()
+{
+	cout << replace_all(string("12212"), "12", "21") << endl;
+	cout << replace_all_distinct(string("12212"), "12", "21") << endl;
+}
